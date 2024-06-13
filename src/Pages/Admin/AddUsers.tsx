1d80dfaed8a4a540a6c619b40d7393e294244d50
vishalmaurya1103 <vishal.maurya1103@gmail.com>
2024-06-13 18:28:15 +0530

    UI Compleated

--- src/Pages/Admin/AddUsers.tsx
+++ src/Pages/Admin/AddUsers.tsx
@@ -1,10 +1,45 @@
-import { Text } from "@chakra-ui/react";
-import React from "react";
+import { Button, Box, Flex, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import React from 'react';
 
 const AddUsers: React.FC = () => {
-    return(
-        <Text>This is Admin Side</Text>
-    )
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box p={20}>
+      <Flex justifyContent='center'>
+        <Button colorScheme='teal' size='lg' onClick={onOpen}>New</Button>
+      </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New User</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl id="name" mb={4}>
+              <FormLabel>Name</FormLabel>
+              <Input placeholder="Enter name" />
+            </FormControl>
+            <FormControl id="email" mb={4}>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="Enter email" />
+            </FormControl>
+            <FormControl id="Designation" mb={4}>
+              <FormLabel>Designation</FormLabel>
+              <Input placeholder="Enter Designation" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Save
+            </Button>
+            <Button variant='ghost' onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
 }
 
-export default AddUsers
+export default AddUsers;
